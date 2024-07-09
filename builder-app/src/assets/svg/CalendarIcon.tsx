b69fc784fb42406e6fc20b01f96ba49b9d2cd8ff
Notorioussensei <52688428+Masterkent11@users.noreply.github.com>
2024-07-08 20:18:23 -0700

    update

--- builder-app/src/assets/svg/CalendarIcon.tsx
+++ builder-app/src/assets/svg/CalendarIcon.tsx
@@ -4,14 +4,17 @@ const CalendarIcon = () => {
   return (
     <div>
       <svg
+        width="16"
+        height="16"
         xmlns="http://www.w3.org/2000/svg"
-        fill="none"
+        fill="#463dbb"
         viewBox="0 0 24 24"
         strokeWidth={1.5}
-        stroke="currentColor"
+        stroke="#463dbb"
         className="size-6  text-[#4f44e0]"
       >
         <path
+          fill="#463dbb"
           strokeLinecap="round"
           strokeLinejoin="round"
           d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5"
